eea94984e6471a95505ed1f88e0046954c7ffee0
simplegr33n <gcgolda@gmail.com>
2017-03-13 19:21:34 -0700

    added categories json

--- app/components/MainActivity/ListView/cats_listview.js
+++ app/components/MainActivity/ListView/cats_listview.js
@@ -1,37 +1,36 @@
 import React, {Component} from 'react';
 import {AppRegistry, Text, View, ListView, StyleSheet, TouchableHighlight} from 'react-native';
 
-const categories = [
-    {category: 'Aboriginal Law'},
-    {category: 'Access to Justice'},
-    {category: 'Administrative Law'},
-    {category: 'Agricultural'},
-    {category: 'Alternative Dispute Resolution'},
-    {category: 'Asian Law'},
-    {category: 'Banking'},
-    {category: 'Bankruptcy'},
-    {category: 'Civil Code'},
-    {category: 'Civil Litigation'},
-    {category: 'Communication, Media, and Technology Law'},
-    {category: 'Computer & Internet'}
-
-]
-
 
 export default class CatsListView extends Component{
     constructor(){
         super();
         const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
         this.state = {
-            catDataSource: ds.cloneWithRows(categories),
+            catsDataSource: ds,
         };
+    }
+
+
+    componentDidMount(){
+        this.fetchCategories();
+    }
+
+    fetchCategories(){
+        fetch('https://raw.githubusercontent.com/pocket-law/canadian-legal-dictionary/master/json/_categories.json')
+            .then((response) => response.json())
+            .then((response) => {
+                this.setState({
+                    catsDataSource: this.state.catsDataSource.cloneWithRows(response)
+                });
+            });
     }
 
     renderRow(category, sectionId, rowId, highlightRow){
             return(
                 <View style={styles.row}>
                     <View>
-                        <Text style={styles.categoryText}>{category.category}</Text>
+                        <Text style={styles.categoryText}>{category.term}</Text>
                     </View>
                 </View>
             )
@@ -41,7 +40,7 @@ export default class CatsListView extends Component{
         return(
             <ListView
                 style={styles.listView}
-                dataSource={this.state.catDataSource}
+                dataSource={this.state.catsDataSource}
                 renderRow={this.renderRow.bind(this)} />
         );
     }
