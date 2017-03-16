3680c2f68eeddec8a0edc13f915707bb05c13b5a
simplegr33n <gcgolda@gmail.com>
2017-03-15 18:26:26 -0700

    json fixes

--- app/components/MainActivity/ListView/main_listview.js
+++ app/components/MainActivity/ListView/main_listview.js
@@ -80,7 +80,7 @@ export default class MainListView extends Component{
     }
 
     fetchTerms(){
-        fetch('https://raw.githubusercontent.com/pocket-law/canadian-legal-dictionary/master/app/components/MainActivity/ListView/json/pocketlaw_dictionary%20-%20Copy.json')
+        fetch('https://raw.githubusercontent.com/pocket-law/canadian-legal-dictionary/master/app/components/MainActivity/ListView/json/pocketlaw_dictionary.json')
             .then((response) => response.json())
             .then((response) => {
                 this.setState({
