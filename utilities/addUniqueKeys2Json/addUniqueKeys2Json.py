11a8e70c4d9431fd987e5f76d2c3088c4efd07ec
simplegr33n <gcgolda@gmail.com>
2017-03-28 12:26:35 -0700

    created utility to add uniqueIDs to each term in dict; added uniqueIDs

---
+++ utilities/addUniqueKeys2Json/addUniqueKeys2Json.py
@@ -0,0 +1,15 @@
+import json
+
+with open('dict.json') as json_data:
+    data = json.load(json_data)
+    
+    mDate = '032817' #set this manually for now
+    i = 0
+    	
+    for term in data['terms']:
+	    i += 1
+	    term['uniqueID'] = mDate + 'n' + str(i)
+	    print(term)
+
+    with open('outputFile.json', 'w') as outfile:
+	    json.dump(data, outfile)
